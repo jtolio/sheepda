c2a0574427dc5b0fd50a1d894e303f26691db862
JT Olds <hello@jtolds.com>
2017-03-20 11:05:30 -0600

    gopherjs support

---
+++ bin/sheepdajs/main.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2017 JT Olds
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/jtolds/sheepda"
+)
+
+func main() {
+	js.Global.Set("sheepda", map[string]interface{}{
+		"eval": Eval,
+	})
+}
+
+func Eval(source string, mode string) (output string, failure string) {
+	prog, err := sheepda.Parse(sheepda.NewStream(
+		bytes.NewReader([]byte(source))))
+	if err != nil {
+		return "", err.Error()
+	}
+
+	if mode == "parse" {
+		return fmt.Sprint(prog.Expr), ""
+	}
+
+	var out bytes.Buffer
+	val, err := sheepda.Eval(sheepda.NewContext(&out, nil),
+		sheepda.NewScopeWithBuiltins(), prog)
+	if err != nil {
+		return "", err.Error()
+	}
+
+	if mode == "output" {
+		return out.String(), ""
+	}
+
+	if mode == "result" {
+		return val.String(), ""
+	}
+
+	return "", "undefined mode"
+}
